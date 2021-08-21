c1231604d4bf6c3d4d74735e21b40232cdac0880
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-08-21 19:07:53 +0200

    feat: add some ugly code to lint + format

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,43 @@
 <template>
-  <Tutorial/>
+  <section>
+      Ugly example that should be linted and formatted properly
+      <div v-for="(fruit, index) in fruits" @click="toggleEat(index)">
+      <!-- :key="fruit.id"  -->
+      <!-- ESlint should complain that the code above is missing a :key -->
+      <span :class="{ 'was-eaten': fruit.eaten }">{{ fruit.name }}</span>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'ToggleFruits',
+  data() {
+            return {
+      fruits: [
+        { id: 1, name: 'banana', eaten: false },
+        { id: 2, name: 'apple', eaten: true },
+        { id: 3, name: 'watermelon', eaten: false },
+      ],
+    }
+  },
+  methods: {
+toggleEat(clickedFruitIndex) {
+      this.fruits = this.fruits.map((fruit) => ({
+        ...fruit,
+        eaten: false,
+        }))
+      return this.$set(this.fruits, clickedFruitIndex, {
+          ...this.fruits[clickedFruitIndex],
+              eaten: true,
+      })
+    },
+  },
+}
 </script>
+
+<style scoped>
+.was-eaten {
+  color: hsl(24, 81.7%, 49.2%);
+}
+</style>
